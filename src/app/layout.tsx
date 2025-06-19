0ad51bc7c8e022737717366f55117916e20b7a24
thelosechanathip <thelosechanathip1996@gmail.com>
2025-06-19 23:22:12 +0700

    edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Inter } from "next/font/google"
 import AntdRegistry from "./AntdRegistry"
 import { ToastContainer } from 'react-toastify'
+import NextTopLoader from 'nextjs-toploader'
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-geist-sans" })
 
@@ -20,6 +21,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AntdRegistry>
+          <NextTopLoader
+            color="#2299DD"          // สีของ progress bar
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}                // ความสูงของ bar (px)
+            crawl={true}
+            showSpinner={false}       // ซ่อนวงกลมหมุนๆ
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD" // เพิ่มเงาเรืองแสง
+          />
           <ToastContainer />
           {children}
         </AntdRegistry>
