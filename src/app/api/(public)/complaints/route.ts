0cbcb73fdbf4e348b521b29875cc802c0f11eac2
thelosechanathip <thelosechanathip1996@gmail.com>
2025-06-19 22:52:48 +0700

    edit

--- src/app/api/(public)/complaints/route.ts
+++ src/app/api/(public)/complaints/route.ts
@@ -3,7 +3,6 @@ import prisma from '@/lib/prisma'
 
 export async function POST(req: Request) {
     try {
-        console.log(process.env.DATABASE_URL)
         const body = await req.json()
         
         const res = await prisma.complaints.create({ data: { ...body } })
