f709083ac79afd023d8ff43f185b495777689cde
thelosechanathip <thelosechanathip1996@gmail.com>
2025-06-26 19:07:20 +0700

    edit

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             path: '/',
-            maxAge: 60 * 60 * 24 * 7,
+            maxAge: 60 * 60 * 8,
             sameSite: 'strict'
         })
 
